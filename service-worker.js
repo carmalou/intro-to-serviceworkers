dd0fff45ea987c431bc8ae599b79992a62cea2c2
carmalou <carmenbourlonlong@gmail.com>
2017-12-18 19:58:35 -0600

    remove extra fluff

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 self.oninstall = function() {
-  console.log("LALALALALA");
+  console.log("install func");
   // globally accessable cache in service worker
   // you can have multiple caches and the same asset
   // asset fetched will be the last one added (last-in, first-out)
@@ -16,17 +16,6 @@ self.oninstall = function() {
 };
 
 self.onfetch = function(event) {
-    console.log("LALALALALA13");
-    console.log(event);
   // this will grab the very last request
   event.respondWith(caches.match(event.request));
-
-  // caches.open('cash').then(function(cache) {
-  //   cache.add(event.request);
-  // });
-
-  // for a specific request you can do
-  // caches.open('cash').then(function(cache) {
-  //   // whatever
-  // });
 };
